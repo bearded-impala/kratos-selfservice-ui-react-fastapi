6892a7ddf776c770e956163102ca2041db7c28b3
Siddhesh More <siddhesh.more@hidglobal.com>
2026-06-02 14:09:59 -0500

    feat(frontend): add Ory's SessionProvider wrapper

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import { SessionProvider } from "@ory/elements-react/client";
 import "@ory/elements-react/theme/styles.css";
 import { router } from "./router";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <SessionProvider baseUrl="">
+      <RouterProvider router={router} />
+    </SessionProvider>
   </StrictMode>,
 );
