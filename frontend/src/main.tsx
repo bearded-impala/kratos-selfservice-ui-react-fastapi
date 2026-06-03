f1ad19c87b49ddbfb2ae031f09b62d7971605bc6
Siddhesh More <siddhesh.more@hidglobal.com>
2026-06-03 05:17:07 +0000

    feat(frontend): add brand css for custom styling

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { SessionProvider } from "@ory/elements-react/client";
 import "@ory/elements-react/theme/styles.css";
+import "./brand.css";
 import { router } from "./router";
 
 createRoot(document.getElementById("root")!).render(
